cae4dfd1fa3a7a484ad19ba9c0906e183edda1fb
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-01 20:07:04 +0200

    Update CodeExecutor.kt

--- app/src/main/java/com/teremu/secretcodes/CodeExecutor.kt
+++ app/src/main/java/com/teremu/secretcodes/CodeExecutor.kt
@@ -1,1 +1,20 @@
+package com.teremu.secretcodes
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+
+object CodeExecutor {
+
+    fun execute(context: Context, code: String) {
+        try {
+            val uri = Uri.parse("tel:$code")
+            val intent = Intent(Intent.ACTION_DIAL, uri)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(context, "Impossible d'exécuter le code", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
