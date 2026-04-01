f4d4c6b7633318db9ed64ecb9c88f19bfde399bd
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-01 20:08:07 +0200

    Update MainActivity.kt

--- app/src/main/java/com/teremu/secretcodes/MainActivity.kt
+++ app/src/main/java/com/teremu/secretcodes/MainActivity.kt
@@ -1,1 +1,28 @@
+package com.teremu.secretcodes
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.teremu.secretcodes.models.SecretCode
+
+class MainActivity : AppCompatActivity() {
+
+    private val codes = listOf(
+        SecretCode("*#06#", "Afficher l'IMEI"),
+        SecretCode("*#0*#", "Menu de test matériel (Samsung)"),
+        SecretCode("*#1234#", "Informations firmware"),
+        SecretCode("*#*#4636#*#*", "Informations téléphone", isDangerous = false)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recycler = findViewById<RecyclerView>(R.id.recyclerView)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = CodeListAdapter(codes) { code ->
+            CodeExecutor.execute(this, code.code)
+        }
+    }
+}
