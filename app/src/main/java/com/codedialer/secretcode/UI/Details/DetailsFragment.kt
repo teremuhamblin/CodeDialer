65e8639c98ec668bd112fc65b223e85b9e4ef01c
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-02 08:33:53 +0200

    Create DetailsFragment.kt

---
+++ app/src/main/java/com/codedialer/secretcode/UI/Details/DetailsFragment.kt
@@ -0,0 +1,28 @@
+package com.codedialer.secretcode.UI.Details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codedialer.secretcode.databinding.DetailsFragmentBinding
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: DetailsFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DetailsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
