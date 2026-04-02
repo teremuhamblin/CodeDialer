905ca1503c611bf504a72374d18fd98c225b26e2
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-02 07:58:02 +0200

    Create CategoryEntity.kt

---
+++ app/src/main/java/com/codedialer/secretcode/Data/Local/entities/CategoryEntity.kt
@@ -0,0 +1,10 @@
+package com.codedialer.secretcode.Data.Local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "categories")
+data class CategoryEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String
+)
