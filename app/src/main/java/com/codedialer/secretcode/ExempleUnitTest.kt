d9d3247cff014005784090310b8b5975c170d33f
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-02 09:43:11 +0200

    Create ExempleUnitTest.kt

---
+++ app/src/main/java/com/codedialer/secretcode/ExempleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.codedialer.secretcode
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
