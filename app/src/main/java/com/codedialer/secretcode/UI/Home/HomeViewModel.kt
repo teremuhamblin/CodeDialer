d4c40959c25ac3d0c56377a03ea4e95922ee3563
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-02 09:20:13 +0200

    Update HomeViewModel.kt

--- app/src/main/java/com/codedialer/secretcode/UI/Home/HomeViewModel.kt
+++ app/src/main/java/com/codedialer/secretcode/UI/Home/HomeViewModel.kt
@@ -3,12 +3,16 @@ package com.codedialer.secretcode.UI.Home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.codedialer.secretcode.Domain.Model.SecretCode
 import com.codedialer.secretcode.Domain.UseCase.GetAllCodesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class HomeViewModel(
+@HiltViewModel
+class HomeViewModel @Inject constructor(
     getAllCodesUseCase: GetAllCodesUseCase
 ) : ViewModel() {
 
-    val codes: LiveData<List<com.codedialer.secretcode.Domain.Model.SecretCode>> =
+    val codes: LiveData<List<SecretCode>> =
         getAllCodesUseCase().asLiveData()
 }
