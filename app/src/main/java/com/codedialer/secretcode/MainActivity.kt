1bb6fee6d526dcc16bbbf593ee98128d9c7a473d
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-02 09:13:55 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/codedialer/secretcode/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.codedialer.secretcode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.codedialer.secretcode.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHost.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+}
